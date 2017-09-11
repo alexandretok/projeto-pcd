13d76062a8349c20fd8fec644a2623114c277003
Alexandre Justino <alexandretok@gmail.com>
2017-09-11 12:00:59 -0300

    Adiciona transformada de hough

--- c/code.c
+++ c/code.c
@@ -7,11 +7,13 @@ double low = 5, ratio = 3;
 
 void canny();
 void gauss(int);
+void hough(float, float, int, float, float);
 
 int main(int argc, char**argv){
 	image = cvLoadImage(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
 	gauss(15);
 	canny();
+	hough(1, CV_PI/180, 50, 30, 10);
 	
 	cvShowImage("imagem", image);
 	cvWaitKey(0);
@@ -24,6 +26,21 @@ void gauss(int weight){
 		printf("\n\nERROR IN: gauss(weight) -> weight should be an ODD number (ex: 1, 3, 5...)\n\n");
 
 	cvSmooth(image, image, CV_GAUSSIAN, weight, weight, 0, 0);
+}
+
+void hough(float rho, float theta, int threshold, float min_line_len, float max_line_gap){
+	CvMemStorage* storage = cvCreateMemStorage(0);
+	CvSeq* lines = 0;
+	lines = cvHoughLines2(image, storage, CV_HOUGH_PROBABILISTIC, rho, theta, threshold, min_line_len, max_line_gap);
+
+	IplImage* image2 = cvCreateImage( cvGetSize(image), 8, 3 );
+	cvCvtColor( image, image2, CV_GRAY2BGR );
+	for( int i = 0; i < lines->total; i++ ) {
+        CvPoint* line = (CvPoint*)cvGetSeqElem(lines,i);
+        cvLine(image2, line[0], line[1], CV_RGB(255,0,0), 2, 8, 0);
+    }
+
+    image = image2;
 }
 
 void canny(){
