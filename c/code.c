11a7c90a9f3aab7e3a7e505e43b81aa3ca4055ff
Alexandre Justino <alexandretok@gmail.com>
2017-09-11 11:26:55 -0300

    Adiciona filtro gaussiano

--- c/code.c
+++ c/code.c
@@ -6,15 +6,24 @@ IplImage* image = 0;
 double low = 5, ratio = 3;
 
 void canny();
+void gauss(int);
 
 int main(int argc, char**argv){
 	image = cvLoadImage(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	gauss(15);
 	canny();
-
-	cvShowImage("rihanna", image);
+	
+	cvShowImage("imagem", image);
 	cvWaitKey(0);
 
 	cvReleaseImage(&image);
+}
+
+void gauss(int weight){
+	if(weight % 2 == 0)
+		printf("\n\nERROR IN: gauss(weight) -> weight should be an ODD number (ex: 1, 3, 5...)\n\n");
+
+	cvSmooth(image, image, CV_GAUSSIAN, weight, weight, 0, 0);
 }
 
 void canny(){
