232382b04e9b5059ec2a73f5bbaee09b218a8059
Alexandre Justino <alexandretok@gmail.com>
2017-09-12 20:21:15 -0300

    Corrige find_lanes()

--- c/code.c
+++ c/code.c
@@ -4,6 +4,7 @@
 // #include <graphics.h>
 
 IplImage* image = 0;
+IplImage* original_image = 0;
 double low = 5, ratio = 3;
 
 void canny();
@@ -11,18 +12,18 @@ void region_of_interest();
 void gauss(int);
 void hough(float, float, int, float, float);
 CvPoint** find_lanes();
+CvPoint polygon[4];
 
 int main(int argc, char**argv){
 	image = cvLoadImage(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-	gauss(15);
+	original_image = cvLoadImage(argv[1], CV_LOAD_IMAGE_UNCHANGED);
+	gauss(11);
 	canny();
 	region_of_interest();
 	hough(1, CV_PI/180, 69, 10, 10);
-
 	find_lanes();
-	// printf(" %d \n", CV_IMAGE_ELEM(image,uchar,0,0));
 	
-	cvShowImage("imagem", image);
+	cvShowImage("imagem", original_image);
 	cvWaitKey(0);
 
 	cvReleaseImage(&image);
@@ -43,11 +44,13 @@ void canny(){
 void region_of_interest(){
 	const CvArr* img = image;
 	IplImage* mask = cvCreateImage(cvGetSize(image), 8, 1);
-
-	CvPoint  curve[]={cvPoint(140,530),  cvPoint(430,330),  cvPoint(530,330),  cvPoint(920,530)};
-	CvPoint* curveArr[]={curve};
-	int      nCurvePts[]={4};
-	int      nCurves=1;
+    polygon[0] = cvPoint(140,530);
+    polygon[1] = cvPoint(430,330);
+    polygon[2] = cvPoint(530,330);
+    polygon[3] = cvPoint(920,530);
+	CvPoint* curveArr[]={polygon};
+	int nCurvePts[]={4};
+	int nCurves=1;
 
 	cvFillPoly(mask, curveArr, nCurvePts, nCurves, CV_RGB(255,255,255), 8, 0);
 	cvAnd(image, mask, image, NULL);
@@ -70,71 +73,56 @@ void hough(float rho, float theta, int threshold, float min_line_len, float max_
 }
 
 CvPoint** find_lanes(){
-
-	cvShowImage("imagem2", image);
+	int upperLimit = polygon[1].y;
 
 	// Finds the first (most to left and top) red pixel on the left side of the image
 	int leftLine[4] = {0, 0, 0, 0};
-	for (int i = 330; i < image->height; ++i){
-		if(leftLine[0] != 0){
-			i = image->height;
-		}
-		else{
-			for (int j = 0; j < image->width/2; ++j){
-				if(CV_IMAGE_ELEM(image,int,i,j) > 0){
-					leftLine[0] = j;
-                	leftLine[1] = i;
-				}
+	for (int i = upperLimit; i < image->height; i++){
+		if(leftLine[0] != 0)
+			break;
+		for (int j = 0; j < image->width/2; j++){
+			if(CV_IMAGE_ELEM(image,unsigned char,i,j) > 0){
+				leftLine[0] = j;
+            	leftLine[1] = i;
 			}
 		}
 	}
-
-	printf("left1: %d %d %d %d\n", leftLine[0], leftLine[1], leftLine[2], leftLine[3]);
-
+	
     // Finds the last (most to right and bottom) red pixel on the left side of the image
-    for (int i = image->height - 1; i > 330; --i){
-    	if(leftLine[2] != 0){
-    		i = 330;
-    	}
-    	else{
-    		for (int j = 0; j < image->width/2; ++j){
-    			if(CV_IMAGE_ELEM(image,int,i,j) > 0){
-    				leftLine[2] = j;
-                	leftLine[3] = i;
-    			}
-    		}
-    	}
+    for (int i = image->height - 1; i > upperLimit; i--){
+    	if(leftLine[2] != 0)
+    		break;
+		for (int j = 0; j < image->width/2; j++){
+			if(CV_IMAGE_ELEM(image,unsigned char,i,j) > 0){
+				leftLine[2] = j;
+            	leftLine[3] = i;
+			}
+		}
     }
 
     // Finds the first (most to left and top) red pixel on the right side of the image
     int rightLine[4] = {0, 0, 0, 0};
-	for (int i = 330; i < image->height; ++i){
-		if(rightLine[0] != 0){
-			i = image->height;
-		}
-		else{
-			for (int j = image->width/2; j < image->width; ++j){
-				if(CV_IMAGE_ELEM(image,int,i,j) > 0){
-					rightLine[0] = j;
-                	rightLine[1] = i;
-				}
+	for (int i = upperLimit; i < image->height; i++){
+		if(rightLine[0] != 0)
+			break;
+		for (int j = image->width/2; j < image->width; j++){
+			if(CV_IMAGE_ELEM(image,unsigned char,i,j) > 0){
+				rightLine[0] = j;
+            	rightLine[1] = i;
 			}
 		}
 	}
 
     // Finds the last (most to right and bottom) red pixel on the right side of the image
-    for (int i = image->height - 1; i > 330; --i){
-    	if(rightLine[2] != 0){
-    		i = 330;
-    	}
-    	else{
-    		for (int j = image->width/2; j < image->width; ++j){
-    			if(CV_IMAGE_ELEM(image,int,i,j) > 0){
-    				rightLine[2] = j;
-                	rightLine[3] = i;
-    			}
-    		}
-    	}
+    for (int i = image->height - 1; i > upperLimit; i--){
+    	if(rightLine[2] != 0)
+    		break;
+		for (int j = image->width/2; j < image->width; j++){
+			if(CV_IMAGE_ELEM(image,unsigned char,i,j) > 0){
+				rightLine[2] = j;
+            	rightLine[3] = i;
+			}
+		}
     }
 
     // Calculates the slopes of each lane line to extrapolate
@@ -155,11 +143,8 @@ CvPoint** find_lanes(){
 
     CvPoint** lines = {leftPointLines, rightPointLines};
 
-    printf("left: %d %d %d %d\n", leftLine[0], leftLine[1], leftLine[2], leftLine[3]);
-    printf("right: %d %d %d %d\n", rightLine[0], rightLine[1], rightLine[2], rightLine[3]);
-
-    cvLine(image, leftPointLines[0], leftPointLines[1], CV_RGB(255,255,255), 2, 8, 0);
-    cvLine(image, rightPointLines[0], rightPointLines[1], CV_RGB(255,255,255), 2, 8, 0);
+    cvLine(original_image, leftPointLines[0], leftPointLines[1], CV_RGB(255,0,0), 4, CV_AA, 0);
+    cvLine(original_image, rightPointLines[0], rightPointLines[1], CV_RGB(255,0,0), 4, CV_AA, 0);
 
     return lines;
 }
